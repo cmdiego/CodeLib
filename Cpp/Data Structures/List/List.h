7c1cae7a694ee1a2640fe7eed53da8eecca21ebf
cmdiego <42076222+cmdiego@users.noreply.github.com>
2024-03-11 19:02:06 -0400

    Repurpose repo

---
+++ Cpp/Data Structures/List/List.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <iostream>
+
+class List_element
+{
+public:
+	List_element(int n, List_element* ptr);
+
+	int data;
+	List_element* next;
+};
+
+class List
+{
+public:
+	List() : head(nullptr), cursor(nullptr) {}
+
+	// TODO: Implement the following two constructors
+	List(const int* arr, int size); // Array -> List, data transfer constructor
+	List(const List& list); // Move constructor
+	~List(); // Destructor
+
+	// Module implementations
+	void prepend(int val);
+
+	void print();
+
+	int getSize() const;
+
+	void printHead();
+
+	List_element* getHead();
+
+
+private:
+	int size = 0;
+	List_element* head;
+	List_element* tail; // TODO: ADD TO CONSTRUCTORS, MAKE SETTER, GETTER, PRINT
+	List_element* cursor;
+};
+
+#pragma once
+
+
+// NOTES:
+
+/*
+// C notation
+// This is public by default
+struct list_element
+{
+	list_element(int n = 0, list_element* ptr = 0) :
+		d(n), next(ptr) {}
+
+	int d;
+	list_element* next;
+};
+
+// OR EQUIVALENTLY C++ notation
+
+class list_element
+{
+public:
+
+	// Constructor: initializes two elements, d and next as 0
+	// What is 0 used for?
+	// The null pointer which in C++11 is better written as nullptr
+
+	list_element(int n = 0, list_element* ptr = 0) :
+		d(n), next(ptr) {}
+
+	int d;
+	list_element* next;
+};
+*/
---
+++ Cpp/Data Structures/List/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "List.h"
+
+int main()
+{
+	/* LIST */
+	List a;
+	a.prepend(10);
+	a.printHead();
+
+	/* DEEP COPY VS. REFERENCIAL COPY */
+	
+}
